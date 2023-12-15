212ea52aa1e7b4be1b876d0d0845b56b0ce6aece
rutvapatel1605 <146053717+rutvapatel1605@users.noreply.github.com>
2023-12-14 22:28:08 -0500

    Update urls.py

--- Final_Project/urls.py
+++ Final_Project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("",include('QuickBite.urls'))
+    path("QuickBite/",include('QuickBite.urls'))
 ]
