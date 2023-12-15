3c8619d1ad0409fa35034b26a04ad4f2b6889b8c
rutvapatel1605 <146053717+rutvapatel1605@users.noreply.github.com>
2023-12-14 21:52:41 -0500

    Update settings.py

--- Final_Project/settings.py
+++ Final_Project/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-9et4js252a!&xmxdy01sj2jepxn@pb9j3eqnk(sgx16^p+cn@4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG","FALSE").lower() == "True"
+DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
